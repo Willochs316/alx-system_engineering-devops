1dea533819d7a6b258fc8af253c9e80b6e557716
Willochs Ojigbo <82782869+Willochs316@users.noreply.github.com>
2023-01-31 21:42:37 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,27 +1,43 @@
+#!/usr/bin/python3
+"""Contains the count_words function"""
 import requests
-import re
-from collections import Counter
 
-def count_words(subreddit, word_list=[]):
-    word_list = [w.lower() for w in word_list]
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+
+def count_words(subreddit, word_list, found_list=[], after=None):
+    '''Prints counts of given words found in hot posts of a given subreddit.
+
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        found_list (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+    '''
+    user_agent = {'User-agent': 'test45'}
+    posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
+                         .format(subreddit, after), headers=user_agent)
+    if after is None:
+        word_list = [word.lower() for word in word_list]
+
+    if posts.status_code == 200:
+        posts = posts.json()['data']
+        aft = posts['after']
+        posts = posts['children']
+        for post in posts:
+            title = post['data']['title'].lower()
+            for word in title.split(' '):
+                if word in word_list:
+                    found_list.append(word)
+        if aft is not None:
+            count_words(subreddit, word_list, found_list, aft)
+        else:
+            result = {}
+            for word in found_list:
+                if word.lower() in result.keys():
+                    result[word.lower()] += 1
+                else:
+                    result[word.lower()] = 1
+            for key, value in sorted(result.items(), key=lambda item: item[1],
+                                     reverse=True):
+                print('{}: {}'.format(key, value))
+    else:
         return
-    
-    data = res.json()
-    articles = [child["data"]["title"] for child in data["data"]["children"]]
-    articles = " ".join(articles).lower()
-    articles = re.sub(r'[^\w\s]', '', articles)
-    words = articles.split()
-    
-    word_count = Counter(words)
-    result = []
-    for word in word_list:
-        if word in word_count:
-            result.append((word, word_count[word]))
-    
-    result = sorted(result, key=lambda x: (-x[1], x[0]))
-    for r in result:
-        print("{}: {}".format(r[0], r[1]))
